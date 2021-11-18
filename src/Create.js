13ab0428d4c9a9b6a133392667882bb215027be8
LIA CANO <lia.cano@gmail.com>
2021-11-18 11:04:07 -0300

    submit events: create an new blog object

--- src/Create.js
+++ src/Create.js
@@ -6,12 +6,19 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Lia')
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author};
+
+        console.log(blog);
+    }
+
 
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
 
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <label>Blog title:</label>
                 <input
                 type="text"
